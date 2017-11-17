12f3c2824b8e2add335fecbf60ac8c50171355ec
mayank <niksbhardy@gmail.com>
2017-11-17 20:20:05 +0530

    EarthquakeApp JSON Parsing added

--- app/src/main/java/com/example/earthquakeapp/EarthquakeActivity.java
+++ app/src/main/java/com/example/earthquakeapp/EarthquakeActivity.java
@@ -1,45 +1,100 @@
 package com.example.earthquakeapp;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
-    public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private String USGS_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+
+    private EarthquakeAdapter mAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case  R.id.menu_setting :
+                startActivity(new Intent(this,SettingActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.earthquake_activity);
+        setContentView(R.layout.activity_earthquake);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<String> earthquakes = new ArrayList<>();
-        earthquakes.add("India");
-        earthquakes.add("Nepal");
-        earthquakes.add("Pakistan");
-        earthquakes.add("Sri Lanka");
-        earthquakes.add("Afghanistan");
-        earthquakes.add("Iraq");
-        earthquakes.add("Iran");
+        QuakeAsyncTask task = new QuakeAsyncTask();
+        task.execute(USGS_URL);
+
+        final ListView earthquakeListView = (ListView)findViewById(R.id.list);
+        mAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
+        earthquakeListView.setAdapter(mAdapter);
+
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Earthquake earthquake = mAdapter.getItem(i);
+                Uri earthquakeUri = Uri.parse(earthquake.getmUrl());
+                Intent webIntent = new Intent(Intent.ACTION_VIEW,earthquakeUri);
+                startActivity(webIntent);
+            }
+        });
 
 
-        // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
-
-        // Create a new {@link ArrayAdapter} of earthquakes
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, earthquakes);
-
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
     }
+    private class QuakeAsyncTask extends AsyncTask<String,Void,List<Earthquake>>{
 
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected List<Earthquake> doInBackground(String... strings) {
+            List<Earthquake> result = QueryUtils.fetchWeatherData(USGS_URL);
+            return result;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(EarthquakeActivity.this);
+            progressDialog.setMessage("Loading...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+            super.onPreExecute();
+        }
+        @Override
+        protected void onPostExecute(List<Earthquake> data) {
+           mAdapter.clear();
+            if(progressDialog.isShowing()){
+                progressDialog.dismiss();
+            }
+
+            if (data != null && !data.isEmpty()){
+                mAdapter.addAll(data);
+            }
+        }
+    }
 
 
 }
